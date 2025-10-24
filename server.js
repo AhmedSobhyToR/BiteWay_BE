26db804b3b954d6c554191a0cf4bb9e09a08d50c
Ahmed Sobhy <105889007+AhmedSobhyToR@users.noreply.github.com>
2025-10-24 14:26:31 +0300

    ready to publish

--- server.js
+++ server.js
@@ -5,23 +5,16 @@ const { connectDB } = require("./db");
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connect to MSSQL
+// Connect DB
 connectDB();
 
 // Routes
-// Example route:
 app.get("/", (req, res) => {
-  res.send("Server is running locally 🚀");
+  res.send("API is running!");
 });
 
-const homeRoutes = require("./routes/home");
-app.use(homeRoutes);
-// Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
